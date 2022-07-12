252a041bb74651aa76c59f464955325d4c17da39
Josh Crawford <joshua@sgroup.com.au>
2022-07-12 16:22:43 +1000

    Translations

---
+++ src/translations/en/stamped-io.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'API Key Public' => 'API Key Public',
+	'Enter the API Key Public from your Stamped.io account.' => 'Enter the API Key Public from your Stamped.io account.',
+	'API Key Private' => 'API Key Private',
+	'Enter the API Key Private from your Stamped.io account.' => 'Enter the API Key Private from your Stamped.io account.',
+	'Store Hash' => 'Store Hash',
+	'Enter the Store Hash (or Store URL) from your Stamped.io account.' => 'Enter the Store Hash (or Store URL) from your Stamped.io account.',
+	'Select an option' => 'Select an option',
+	'Product Image' => 'Product Image',
+	'Select the asset field used for your product image. This can be an asset field on the product or individual variant.' => 'Select the asset field used for your product image. This can be an asset field on the product or individual variant.',
+	'Product Image Transform' => 'Product Image Transform',
+	'Select an Image Transform for the Product Image to use.' => 'Select an Image Transform for the Product Image to use.',
+];
