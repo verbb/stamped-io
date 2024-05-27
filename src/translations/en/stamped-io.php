63ea56ab4d7c90ae3bb812c62f3686d2f2d918c3
Josh Crawford <joshua@sgroup.com.au>
2024-05-27 15:16:10 +1000

    Add English translations

--- src/translations/en/stamped-io.php
+++ src/translations/en/stamped-io.php
@@ -1,4 +1,15 @@
 <?php
 
 return [
+  'API Key Private' => 'API Key Private',
+  'API Key Public' => 'API Key Public',
+  'Enter the API Key Private from your Stamped.io account.' => 'Enter the API Key Private from your Stamped.io account.',
+  'Enter the API Key Public from your Stamped.io account.' => 'Enter the API Key Public from your Stamped.io account.',
+  'Enter the Store Hash (or Store URL) from your Stamped.io account.' => 'Enter the Store Hash (or Store URL) from your Stamped.io account.',
+  'Product Image' => 'Product Image',
+  'Product Image Transform' => 'Product Image Transform',
+  'Select an Image Transform for the Product Image to use.' => 'Select an Image Transform for the Product Image to use.',
+  'Select an option' => 'Select an option',
+  'Select the asset field used for your product image. This can be an asset field on the product or individual variant.' => 'Select the asset field used for your product image. This can be an asset field on the product or individual variant.',
+  'Store Hash' => 'Store Hash',
 ];
